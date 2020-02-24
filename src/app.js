610ccb20b43df4b18dbea87984ec17c1cf040273
David Vloedman <djvloedman@gmail.com>
2020-02-24 15:04:03 -0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// State Only
+import ToDo from "./components/todo/todo.js";
+
+// API Connected (Live Data)
+
+export default function App() {
+  return (
+    <>
+      <ToDo />
+    </>
+  );
+}
---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>There are better to do apps out there</p>
+    </footer>
+  );
+}
+
+export default Footer;
